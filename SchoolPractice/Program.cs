caa543b106c2aab0e8a2421d578c7fa164cc9d0d
dennis-phillips <dennis_phillips@outlook.com>
2020-06-04 10:03:14 -0400

    Unit 4 Exercises

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SchoolPractice
 {
@@ -6,7 +7,23 @@ namespace SchoolPractice
     {
         public static void Main(string[] args)
         {
+            Student dennis = new Student("Dennis",1,1,4.0);
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
         }
+
+    }
+
+    public class Course
+    {
+        private string courseTitle {get; set;}
+        private List <Student> roster {get; set;}
+        private List <int> grades {get; set;}
+     }
+    public class Teacher
+    {
+        private string FirstName {get; set;}
+        private string LastName {get; set;}
+        private string Subject {get;set;}
+        private int YearsTeaching {get; set;}
     }
 }
