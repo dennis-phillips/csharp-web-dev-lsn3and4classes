0e3cf20c3f3e9aba13c54e8b08e1f2fd8de2f0c3
dennis-phillips <dennis_phillips@outlook.com>
2020-06-07 16:23:11 -0400

    Exercises complete

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -10,11 +10,15 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Test your exercise solutions with print statements here.
-            public Student dennis = new Student("Dennis", 1, 30, 3.0 );
-            public Student moe = new Student("Moe", 2, 70, 3.0);
+            Student dennis = new Student("Dennis", 1, 30, 3.0);
+            Student moe = new Student("Moe", 2, 70, 3.0);
+            Console.WriteLine(moe.ToString());
+            moe.AddGrade(4, 4.0);
+            Console.WriteLine(moe.GetGradeLevel(moe.NumberOfCredits));
         
-            
+        
 
         }
-    } 
+    }
+    
 }
--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace SchoolPractice
 {
     public class Student
